5f9757ef8f56022b685520466436539538a98fcd
liuyang <515939422@qq.com>
2021-03-25 17:47:32 +0800

    update：手写router

--- API/手写API.js
+++ API/手写API.js
@@ -91,4 +91,18 @@ Function.prototype.call2 = function(obj) {
 
 }
 
+// 手写 instanceof
+function myInstanceof(source, compare) {
+    // 基本数据类型直接返回 false
+    if(typeof source !== 'object' || source === null) return false
+    //getProtypeOf是Object对象自带的一个方法，能够拿到参数的原型对象
+    let proto = Object.getPrototypeOf(source)
+    while(true) {
+        //查找到尽头，还没找到
+        if(proto === null) return false
+        //找到相同的原型对象
+        if(proto === compare.prototype) return true
+        proto = Object.getPrototypeOf(proto)
+    }
+}
 
