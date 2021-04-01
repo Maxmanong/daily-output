e523c7987d154386de3a37524aeea555aca258a2
liuyang <515939422@qq.com>
2021-04-01 22:48:29 +0800

    update：日常更新

--- 封装配置文件/webpack运行流程.js
+++ 封装配置文件/webpack运行流程.js
@@ -14,3 +14,16 @@
 // 初始化： 启动构建， 读取与合并配置参数， 加载 Plugin， 实例化 Compiler
 // 编译： 从 Entry 出发， 针对每个 Module 串行调用对应的 Loader 去翻译文件的内容， 再找到该 Module 依赖的 Module， 递归地进行编译处理
 // 输出： 将编译后的 Module 组合成 Chunk， 将 Chunk 转换成文件， 输出到文件系统中
+
+
+//  Webpack 的热更新原理吧
+//      (敲黑板， 这道题必考)
+//  Webpack 的热更新又称热替换（ Hot Module Replacement）， 缩写为 HMR。 
+//  这个机制可以做到不用刷新浏览器而将新变更的模块替换掉旧的模块。
+//  HMR的核心就是客户端从服务端拉去更新后的文件， 准确的说是 chunk diff(chunk 需要更新的部分)，
+//  实际上 WDS 与浏览器之间维护了一个 Websocket， 当本地资源发生变化时， WDS 会向浏览器推送更新，
+//  并带上构建时的 hash， 让客户端与上一次资源进行对比。 
+//  客户端对比出差异后会向 WDS 发起 Ajax 请求来获取更改内容(文件列表、 hash)，
+//  这样客户端就可以再借助这些信息继续向 WDS 发起 jsonp 请求获取该chunk的增量更新。
+//  后续的部分(拿到增量更新之后如何处理？ 哪些状态该保留？ 哪些又需要更新？) 由 HotModulePlugin 来完成，
+//  提供了相关 API 以供开发者针对自身场景进行处理， 像react - hot - loader 和 vue - loader 都是借助这些 API 实现 HMR。
