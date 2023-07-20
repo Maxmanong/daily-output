82fcd9e32cc6f1fccaee860f2d38cbf7f2a0d669
柳阳 <515939422@qq.com>
2023-07-20 17:32:03 +0800

    test：测试提交 --20230720

--- 原生JS/手写Promise.js
+++ 原生JS/手写Promise.js
@@ -1,137 +1,137 @@
 function MyPromise(executor) {
-    let self = this
-    self.value = null
-    self.error = null
-    self.status = 'PENDING'
+    let self = this;
+    self.value = null;
+    self.error = null;
+    self.status = 'PENDING';
 
 
-    self.onFulfilledCallbacks = []
-    self.onRejectedCallbacks = []
+    self.onFulfilledCallbacks = [];
+    self.onRejectedCallbacks = [];
 
     const resolve = val => {
-        if (self.status !== 'PENDING') return
+        if (self.status !== 'PENDING') return;
 
         setTimeout(() => {
-            self.status = 'FUFILLED'
-            self.value = val
+            self.status = 'FULFILLED';
+            self.value = val;
             self.onFulfilledCallbacks.forEach(callback => {
-                callback(self.value)
-            })
+                callback(self.value);
+            });
         }, 0);
-    }
+    };
 
     const reject = err => {
-        if(self.status !== 'PENDING') return
+        if(self.status !== 'PENDING') return;
 
         setTimeout(() => {
-            self.status = 'REJECTED'
-            self.error = err
+            self.status = 'REJECTED';
+            self.error = err;
             self.onRejectedCallbacks.forEach(callback => {
-                callback(self.error)
-            })
+                callback(self.error);
+            });
         }, 0);
-    }
-    executor(resolve, reject)
+    };
+    executor(resolve, reject);
 }
 
 function resolvePromise(bridgePromise, x, resolve, reject) {
     if(x instanceof MyPromise) {
         if (x.status === 'PENDING') {
-            x.then(y => { resolvePromise(bridgePromise, y, resolve, reject) }, error => {  reject(error) })
+            x.then(y => { resolvePromise(bridgePromise, y, resolve, reject); }, error => {  reject(error); });
         } else {
-            x.then(resolve, reject)
+            x.then(resolve, reject);
         }
     } else {
-        resolve(x)
-    }
-}
+        resolve(x);
+    };
+};
 
 MyPromise.prototype.then = function(onFulfilled, onRejected) {
-    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
-    onRejected = typeof onRejected === 'function' ? onRejected : error => { throw error }
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    onRejected = typeof onRejected === 'function' ? onRejected : error => { throw error };
     console.log('onRejected: ', onRejected);
-    let bridgePromise, self = this
+    let bridgePromise, self = this;
     if (self.status === 'PENDING') {
         return bridgePromise = new MyPromise((resolve, reject) => {
             self.onFulfilledCallbacks.push((value) => {
                 try {
-                    let x = onFulfilled(value)
-                    resolvePromise(bridgePromise, x, resolve, reject)
+                    let x = onFulfilled(value);
+                    resolvePromise(bridgePromise, x, resolve, reject);
                 } catch(e) {
-                    reject(e)
-                }
-            })
+                    reject(e);
+                };
+            });
             console.log('onFulfilledCallbacks:', self.onFulfilledCallbacks[0]);
             self.onRejectedCallbacks.push((error) => {
                 try {
-                    let x = onRejected(error)
-                    resolvePromise(bridgePromise, x, resolve, reject)
+                    let x = onRejected(error);
+                    resolvePromise(bridgePromise, x, resolve, reject);
                 } catch(e) {
-                    reject(e)
-                }
-            })
-        })
-    } else if(this.status === 'FUFILLED') {
-        console.log('------------FUFILLED--------');
+                    reject(e);
+                };
+            });
+        });
+    } else if(this.status === 'FULFILLED') {
+        console.log('------------FULFILLED--------');
         return bridgePromise = new MyPromise((resolve, reject) => {
             try {
-                let x = onFulfilled(self.value)
-                resolvePromise(bridgePromise, x, resolve, reject)
+                let x = onFulfilled(self.value);
+                resolvePromise(bridgePromise, x, resolve, reject);
             } catch(e) {
-                reject(e)
-            }
-        })
+                reject(e);
+            };
+        });
         
     } else if(self.status === 'REJECTED') {
         console.log('------------REJECTED--------');
         return bridgePromise = new MyPromise((resolve, reject) => {
             try {
-                let x = onRejected(self.error)
-                resolvePromise(bridgePromise, x, resolve, reject)
+                let x = onRejected(self.error);
+                resolvePromise(bridgePromise, x, resolve, reject);
             } catch(e) {
-                reject(e)
-            }
-        })
-    }
-    return this
-}
+                reject(e);
+            };
+        });
+    };
+    return this;
+};
 
 function MP1() {
     let p1 = new MyPromise((resolve, reject) => {
-        let num = parseInt(Math.random() * 100)
+        let num = parseInt(Math.random() * 100);
         setTimeout(() => {
             if(num >= 50) {
-                resolve(num)
+                resolve(num);
             } else {
-                reject(num)
-            }
+                reject(num);
+            };
         }, 3000);
-    })
-    return p1
-}
+    });
+    return p1;
+};
 function MP2() {
     let p2 = new MyPromise(function (resolve, reject) {
-        let num2 = parseInt(Math.random() * 1000)
+        let num2 = parseInt(Math.random() * 1000);
         setTimeout(() => {
             if (num2 >= 500) {
-                resolve(num2)
+                resolve(num2);
             } else {
-                reject(num2)
-            }
-        }, 3000)
-    })
-    return p2
+                reject(num2);
+            };
+        }, 3000);
+    });
+    return p2;
 }
 MP1()
 .then(value => {
-    console.log('大于或者等于50的num值为: ', value)
-    return MP2()
+    console.log('大于或者等于50的num值为: ', value);
+    return MP2();
 }, error => {
-    console.log('小于50的num为: ', error)
-    return MP2()
+    console.log('小于50的num为: ', error);
+    return MP2();
 })
 .then(value => {
-    console.log('大于或者等于500的num2值为: ', value)
+    console.log('大于或者等于500的num2值为: ', value);
 }, error => {
-    console.log('小于500的num2为: ', error)
-})
+    console.log('小于500的num2为: ', error);
+});
